a65c2d4e4546a64f7c452c178e397465681ea4b7
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-09 11:18:59 -0600

    Update findreplace.java

--- CSE2130/src/findandreplace/findreplace.java
+++ CSE2130/src/findandreplace/findreplace.java
@@ -17,34 +17,41 @@ public class findreplace
 {
 	public static void main(String[] args) 
 	{
-		String fName, findText,replacedText;
+		String fileName, searchText, replaceText;
 		//create variables
-		Scanner input = new Scanner(System.in);
-		BufferedReader readFile;
-		FileReader in;
-		String replaced = null;
+		try (Scanner input = new Scanner(System.in))
+		{
+			BufferedReader readFile;
+			FileReader in;
+			String replaced;
 		
-		System.out.print("Please enter file name:");
-		//test file name is Zackers.txt, type in Zackers.txt
-		fName = input.nextLine();
-		File tFile = new File(fName);
-		//look for text file
+			System.out.print("Please enter file name:");
+			//test file name is Zackers.txt, type in Zackers.txt
+			fileName = input.nextLine();
+			File tFile = new File(fileName);
+			//look for text file
+			
 		try
 		{
-			System.out.println("What would you like to replace? (Letters only)");
+			System.out.println("What would you like to replace? (only words from the list below)");
+			System.out.println("bagel, red, orange, banana, potato, word, green, lime.");
 			//prompt for the letter or word the user would like to replace
-			findText = input.nextLine();
+			//the file consists of "bagel red orange banana potato word green lime"
+			searchText = input.nextLine();
+			
 			System.out.println("What would you like to replace it with?");
 			//prompt for the letter or word to replace that letter or word with.
-			replacedText = input.nextLine();
+			replaceText = input.nextLine();
 			//replace text
+			
 			in = new FileReader(tFile);
 			readFile = new BufferedReader(in);
 			
-			replacedText = readFile.readLine();
+			replaced = readFile.readLine();
 			System.out.println(replaced);
-			System.out.println("Replacing " + findText + " with " + replacedText);
-			System.out.println(replaced.replaceAll(findText, replacedText));
+			
+			System.out.println("Now replacing " + searchText + " with " + replaceText);
+			System.out.println(replaced.replaceAll(searchText, replaceText));
 			
 			readFile.close();
 			in.close();
@@ -52,7 +59,7 @@ public class findreplace
 		}
 		catch(FileNotFoundException e)
 		{
-			System.out.println("File doesn't exist.");
+			System.out.println("This file doesn't exist!");
 			System.err.println("FileNotFoundException: "+ e.getMessage());
 			
 		}
@@ -62,15 +69,25 @@ public class findreplace
 			System.err.println("IOException: "+ e.getMessage());
 			
 		}
+		}
 		
 	}
+	
 
 }	
 
 
 /* Screen Dump
 
-
+Please enter file name:Zackers.txt
+What would you like to replace? (only words from the list below)
+bagel, red, orange, banana, potato, word, green, lime.
+bagel
+What would you like to replace it with?
+milk
+bagel red orange banana potato word green lime
+Now replacing bagel with milk
+milk red orange banana potato word green lime
 
 
  */
