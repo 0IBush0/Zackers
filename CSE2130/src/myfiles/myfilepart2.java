150587b839bedfddde7127f949ce033345912dd0
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-09 11:32:50 -0600

    Update myfilepart2.java

--- CSE2130/src/myfiles/myfilepart2.java
+++ CSE2130/src/myfiles/myfilepart2.java
@@ -1,5 +1,5 @@
 /*
-Program: myfilepart2.java          Last Date of this Revision: April 26, 2022
+Program: myfilepart2.java          Last Date of this Revision: May 9, 2022
 Purpose: My File P2
 Author: Zac Qiu
 School: CHHS
@@ -9,7 +9,6 @@ Course: Computer Programming 30
 
 package myfiles;
 
-import java.awt.event.ActionEvent;
 import java.io.*;
 import java.util.Scanner; 
 
@@ -17,41 +16,42 @@ public class myfilepart2
 {
 	    File textFile = new File("Bread.txt");//file name Bread.txt
 		{
-		Scanner input = new Scanner(System.in);
+			Scanner input = new Scanner(System.in);
+			try
+			{
+				textFile.createNewFile();
+				System.out.println("New file created.");
+				//prompt new file message
+			}
+			catch(IOException e)
+			{
+				System.out.println("File cannot be created.");
+				System.err.println("IOException: " + e.getMessage());
+				//check for exceptions, if error, do not create file
+			}
 
-		try
-		{
-			textFile.createNewFile();
-			System.out.println("New file created.");
-			//prompt new file message
-		}
-		catch(IOException e)
-		{
-			System.out.println("File cannot be created.");
-			System.err.println("IOException: " + e.getMessage());
-			//check for exceptions, if error, do not creare file
-		}
-
-		System.out.println("n\\ Save this File \\ Delete this File");
-		String action = input.next();
-		String SF;
-		String DF;
+			System.out.println("Save or Delete?");
+			String action = input.next();
+			String Save;
+			String Delete;
 		
-		if(action.equalsIgnoreCase("SF"))
-		{
-			textFile.exists();
-			System.out.println("File saved.");
-			//display save message
-		}
-		else if(action.equalsIgnoreCase("DF"))
-		{
-			textFile.delete();
-			System.out.println("File deleted.");
-			//display delete message
-		}
+			if(action.equalsIgnoreCase("Save"))
+			{
+				textFile.exists();
+				System.out.println("File saved.");
+				//display save message
+			}
+			else if(action.equalsIgnoreCase("Delete"))
+			{
+				textFile.delete();
+				System.out.println("File deleted.");
+				//display delete message
+			}
+			
 	    }
 	    
 }
+
 /* Screen Dump
 
 
