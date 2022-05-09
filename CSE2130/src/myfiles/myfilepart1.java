b9ad7f1de93aaccf82f3b2cbc9ce07b605326ba8
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-09 11:28:26 -0600

    Update myfilepart1.java

--- CSE2130/src/myfiles/myfilepart1.java
+++ CSE2130/src/myfiles/myfilepart1.java
@@ -1,5 +1,5 @@
 /*
-Program: myfilepart1.java          Last Date of this Revision: April 26, 2022
+Program: myfilepart1.java          Last Date of this Revision: May 9, 2022
 Purpose: My File P1
 Author: Zac Qiu
 School: CHHS
@@ -14,31 +14,34 @@ import java.util.Scanner;
 
 public class myfilepart1
 {
-	public static void main(String[] args)
+	
+	public static void main(String[] args) 
 	{
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the name of your file: ");
-		String files = input.next();
-		//prompt for user text
-		File textfile = new File("c:\\temp\\supplies.txt");
-		if (textfile.exists())
+		Scanner input = new Scanner(System.in);		
+		System.out.print("Please Enter the name of a file: ");
+		//prompt for file name, file name is "Bread.txt"
+		String fileName = input.next();
+		File textFile = new File(fileName);
+		
+		if(textFile.exists())
 		{
 			System.out.println("File already exists.");
-			//display exist message
+			//file existing message
 		}
 		else
 		{
 			System.out.println("File does not exist.");
-			//display none existent message
+			//file does not exist message
 		}
 		
-	}
-	
+	 }
+
 }
 
 /* Screen Dump
 
-
+Please Enter the name of a file: Bread.txt
+File already exists.
 
 
  */
