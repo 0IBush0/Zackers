012b35865281d71c330f31e530a8d8077b335599
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-17 09:31:11 -0600

    Add files via upload

---
+++ CSE3110/src/squares/squares.java
@@ -0,0 +1,22 @@
+/*
+Program: .java          Last Date of this Revision: May 16, 2022
+Author: Zac Qiu
+School: CHHS
+Course: Computer Programming 30
+ 
+*/
+
+package squares;
+
+
+public class squares
+{
+	
+}
+
+/* Screen Dump
+
+
+
+
+ */
