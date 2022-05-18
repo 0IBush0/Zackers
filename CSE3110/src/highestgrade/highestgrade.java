96feb975bc8ce2ec005530320dc83f2b7465c3d3
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-18 11:01:03 -0600

    Update highestgrade.java

--- CSE3110/src/highestgrade/highestgrade.java
+++ CSE3110/src/highestgrade/highestgrade.java
@@ -1,5 +1,5 @@
 /*
-Program: .java          Last Date of this Revision: May 16, 2022
+Program: highestgrade.java        Last Date of this Revision: May 18, 2022
 Author: Zac Qiu
 School: CHHS
 Course: Computer Programming 30
@@ -9,14 +9,51 @@ Course: Computer Programming 30
 package highestgrade;
 
 
+import java.util.Scanner;
+
 public class highestgrade
 {
-	
+	public static void main(String[] args)
+	{
+		double scorValue = 0;
+		double lowScore = 100;
+		double higScore = 0;
+		int num = 0;
+		int[] grade = new int[3];
+		//ask 3 times
+		Scanner input = new Scanner(System.in);
+
+	    for(int i = 0; i < grade.length;i ++)
+	    {
+			 System.out.println("Please enter a grade between 0-100: ");
+			 //prompt for a number between 0-100
+			 num = input.nextInt();
+			 grade[i] = num;
+
+	    }
+	    for(int i = 0; i < grade.length;i ++)
+	    {
+			 if(num > higScore)//check value
+		     {
+			     higScore = num;
+
+		     }
+			 
+	    }
+	    	System.out.println("The highest score: " + higScore);
+	    	//display the highest grade
+	}
+
 }
 
 /* Screen Dump
 
-
+Please enter a grade between 0-100: 
+20
+Please enter a grade between 0-100: 
+30
+Please enter a grade between 0-100: 
+40
 
 
  */
