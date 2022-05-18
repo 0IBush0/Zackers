d00012133f491f70ab38694d2a01b46f5668581d
IBush <100794571+0IBush0@users.noreply.github.com>
2022-05-18 11:01:35 -0600

    Update squares.java

--- CSE3110/src/squares/squares.java
+++ CSE3110/src/squares/squares.java
@@ -1,5 +1,5 @@
 /*
-Program: .java          Last Date of this Revision: May 16, 2022
+Program: squares.java          Last Date of this Revision: May 18, 2022
 Author: Zac Qiu
 School: CHHS
 Course: Computer Programming 30
@@ -8,15 +8,38 @@ Course: Computer Programming 30
 
 package squares;
 
+import java.util.Scanner;
+
 
 public class squares
 {
-	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int[] SquareR = new int[5];
+		//5 elements will be displayed
+	    for(int i = 0; i < SquareR.length;i ++)
+	    {
+	    	SquareR[i] = i * i;
+	    	
+	    }
+	    for(int i = 0; i < SquareR.length;i ++)
+	    {
+	    	System.out.println(SquareR[i]);
+
+	    }
+
+ 	}
 }
+
 
 /* Screen Dump
 
-
+0
+1
+4
+9
+16
 
 
  */
