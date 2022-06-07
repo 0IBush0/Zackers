c56103488aa9b4b235f76d29b1d94656719e201b
IBush <100794571+0IBush0@users.noreply.github.com>
2022-06-07 08:56:06 -0600

    Update hotorcold.java

--- GettingStarted/src/hotorcold.java
+++ GettingStarted/src/hotorcold.java
@@ -1,21 +1,51 @@
-import com.phidget22.HumiditySensor;
+import com.phidget22.DigitalOutput;
 import com.phidget22.TemperatureSensor;
+public class hotorcold
+{
+	public static void main(String[] args) throws Exception
+	 {
+	     TemperatureSensor temperatureSensor = new TemperatureSensor();
+	     temperatureSensor.setHubPort(3);
+	     DigitalOutput greenLED = new DigitalOutput();
+	     greenLED.setHubPort(1);
+	     greenLED.setIsHubPortDevice(true);
+	     DigitalOutput redLED = new DigitalOutput();
+	     redLED.setHubPort(2);
+	     redLED.setIsHubPortDevice(true);
+	     
+	     temperatureSensor.open(1000);
+	     redLED.open(1000);
+	     greenLED.open(1000);
+	     
+	     while(true)
+	     {
+	    	 if (temperatureSensor.getTemperature() > 19)//if more than 19, turn green
+	    	 {
+	    		 greenLED.setState(true);
+	    		 int T = (int) temperatureSensor.getTemperature();
+	    		 System.out.println("Temperature: " + T + "°C");
+	    		 Thread.sleep(1000);
+	    		 
+	    	 }
+	    	 if (temperatureSensor.getTemperature() < 25)//if less than 25, turn green
+	    	 {
+	    		 greenLED.setState(true);
+	    		 int T = (int) temperatureSensor.getTemperature();
+	    		 System.out.println("Temperature: " + T + "°C");
+	    		 Thread.sleep(1000);
+	    		 
+	    	 }
+	    	 else
+	    	 {
+	    		 redLED.setState(true);//if more or less, turn red
+	    		 int T = (int) temperatureSensor.getTemperature();
+	    		 System.out.println("Temperature: " + T + "°C");
+	    		 Thread.sleep(1000);
+	    		 
+	    	 }
+	    	 
+	     }
 
-public class hotorcold {
-    public static void main(String[] args) throws Exception{
-
-        //Create 
-        HumiditySensor humiditySensor = new HumiditySensor();
-        TemperatureSensor temperatureSensor = new TemperatureSensor();
-
-        //Open 
-        humiditySensor.open(1000);
-        temperatureSensor.open(1000);
-
-        //Use your Phidgets 
-        while(true){
-            System.out.println("Humidity: " + humiditySensor.getHumidity() +" %RH, Temperature: " + temperatureSensor.getTemperature() + " �C" );
-            Thread.sleep(150);
-        }
-    }
+	 }
+	
 }
